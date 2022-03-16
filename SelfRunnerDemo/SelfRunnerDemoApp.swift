6868fa96157f98af6d8995d05643788c92026987
Jacob Bullock <jacob.bullock@gmail.com>
2022-03-16 11:16:39 -0700

    init commit

---
+++ SelfRunnerDemo/SelfRunnerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfRunnerDemoApp.swift
+//  SelfRunnerDemo
+//
+//  Created by Jacob Bullock on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SelfRunnerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
